7b57f1794a4ce08333dcd01fcfbd3625c687d22f
Wilberth Gonzalez <wilbertg@uci.edu>
2025-07-12 14:37:23 -0700

    Add Vec2 class for 2D vector representation and basic operations

---
+++ src/geometry/Vector2.js
@@ -0,0 +1,23 @@
+/**
+ * Represents a 2D vector with x and y components.
+ *
+ * @class Vec2
+ */
+export class Vec2 {
+    constructor(x = 0, y = 0) {
+        this.x = x;
+        this.y = y;
+    }
+
+    add(vec) {
+        return new Vec2(this.x + vec.x, this.y + vec.y);
+    }
+
+    sub(vec) {
+        return new Vec2(this.x - vec.x, this.y - vec.y);
+    }
+
+    scale(scalar) {
+        return new Vec2(this.x * scalar, this.y * scalar);
+    }
+}
