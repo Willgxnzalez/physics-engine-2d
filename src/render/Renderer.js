c1c17d8f994eeedab7ab5d69c65b1f3ad6eb7793
Wilberth Gonzalez <wilbertg@uci.edu>
2025-07-12 14:31:21 -0700

    Create starter files and basic Renderer

---
+++ src/render/Renderer.js
@@ -0,0 +1,16 @@
+export class Renderer {
+    static render(scene, bodies) {
+        ctx.strokeStyle = 'white';
+        ctx.lineWidth = 1;
+
+        for (const body of bodies) {
+            ctx.beginPath();
+            ctx.moveTo(body.vertices[0].x, body.vertices[0].y);
+            for (let i = 1; i < body.vertices.length; i++) {
+                ctx.lineTo(body.vertices[i].x, body.vertices[i].y);
+            }
+            ctx.closePath();
+            ctx.stroke();
+        }
+    }
+}
