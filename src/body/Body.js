1c24eba5061dde225cf7e7c701129979abd93ac8
Wilberth Gonzalez <wilbertg@uci.edu>
2025-07-13 17:18:09 -0700

    Create RigidBody Representation and add Engine update loop

---
+++ src/body/Body.js
@@ -0,0 +1,38 @@
+/**
+ * A rigid body in 2D space.
+ */
+
+import { Vec2 } from "../geometry/Vec2.js";
+import { Bounds } from "../geometry/Bounds.js";
+import { Vertices } from "../geometry/Vertices.js";
+
+export class Body {
+    constructor({
+        position = new Vec2(0, 0),
+        velocity = new Vec2(0, 0),
+        isStatic = false,
+        mass = 1,
+        vertices = []
+    } = {}) {
+        this.position = position.clone();
+        this.velocity = velocity.clone();
+
+        this.isStatic = !!isStatic;
+        this.mass = this.isStatic ? Infinity : mass;
+
+        this.vertices = vertices;
+        this.bounds = Bounds.fromVertices(this.vertices);
+    }
+
+    update(dt, gravity) {
+        if (this.isStatic) return;
+
+        this.velocity.y += gravity.y * dt;
+
+        const displacement = this.velocity.scale(dt);
+        this.position.translate(displacement);
+
+        Vertices.translate(this.vertices, displacement);
+        this.bounds.translate(displacement);
+    }
+}
---
+++ src/core/Engine.js
@@ -0,0 +1,17 @@
+
+export class Engine {
+    constructor() {
+        this.bodies = [];
+        this.gravity = 50
+    }
+
+    addBody(body) {
+        this.bodies.push(body);
+    }
+
+    update(dt) {
+        for (const body of this.bodies) {
+            body.udpate(dt, this.gravity);
+        }
+    }
+}
