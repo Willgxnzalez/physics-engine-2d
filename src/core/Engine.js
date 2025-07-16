0c4df23503b0ca56828602961e9b2e9b73859f3d
Wilberth Gonzalez <wilbertg@uci.edu>
2025-07-15 23:27:22 -0700

    Add methods for dynamically adding and removing forces to the update loop

--- src/core/Engine.js
+++ src/core/Engine.js
@@ -1,17 +1,65 @@
+import { Vec2 } from '../geometry/Vec2.js';
 
 export class Engine {
-    constructor() {
-        this.bodies = [];
-        this.gravity = 300; // Gravity acceleration in pixels per second squared
-    }
+  constructor() {
+    this.bodies = [];
+    this.gravityEnabled = true;
+    this.gravityStrength = 100;
+    this.gravityDirection = new Vec2(0, 1); // Downward unit vector
+    this.forces = [];
+    
 
-    addBody(body) {
-        this.bodies.push(body);
-    }
+    this.gravityForce = (body) => {
+      if (this.gravityEnabled && !body.isStatic) 
+        return this.gravityDirection.scale(this.gravityStrength * body.mass);
+      return null;
+    };
+    this.addForce(this.gravityForce);
+  }
 
-    update(dt) {
-        for (const body of this.bodies) {
-            body.update(dt, this.gravity);
+  addBody(body) {
+    this.bodies.push(body);
+  }
+
+  addForce(forceFunction) {
+    this.forces.push(forceFunction);
+  }
+
+  removeForce(forceFunction) {
+    const index = this.forces.indexOf(forceFunction);
+    if (index > -1) {
+      this.forces.splice(index, 1);
+    }
+  }
+
+  enableGravity() {
+    this.gravityEnabled = true;
+  }
+
+  disableGravity() {
+    this.gravityEnabled = false;
+  }
+
+  setGravityStrength(strength) {
+    this.gravityStrength = strength;
+  }
+
+  setGravityDirection(directionVec2) {
+    this.gravityDirection = directionVec2.normalize();
+  }
+
+  update(dt) {
+    for (const forceFunction of this.forces) {
+      for (const body of this.bodies) {
+        const force = forceFunction(body);
+        if (force) {
+          body.applyForce(force);
         }
+      }
     }
+
+    for (const body of this.bodies) {
+      body.update(dt);
+    }
+  }
 }
